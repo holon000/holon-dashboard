3ff78a391c0d141bdaf29a06186b5c72f1d8ab72
Taylor Gerring <taylor.gerring@gmail.com>
2014-02-13 02:02:05 +0100

    Enable click-to-expand

--- assets/linker/js/main.js
+++ assets/linker/js/main.js
@@ -2,11 +2,13 @@
 // http://draggabilly.desandro.com/draggabilly.pkgd.js added as external resource
 
 docReady( function() {
-  var container = document.querySelector('.packery');
+  var container = document.querySelector('#widgets.packery');
+
   var pckry = new Packery( container, {
     columnWidth: 24,
     rowHeight: 24
   });
+
   var itemElems = pckry.getItemElements();
   // for each item element
   for ( var i=0, len = itemElems.length; i < len; i++ ) {
@@ -16,4 +18,27 @@ docReady( function() {
     // bind Draggabilly events to Packery
     pckry.bindDraggabillyEvents( draggie );
   }
+
+  eventie.bind( container, 'click', function( event ) {
+    // don't proceed if item was not clicked on
+    var target = event.target;
+    if ( !classie.has( target, 'item' ) ) {
+      return;
+    }
+
+    var isGigante = classie.has( target, 'gigante' );
+    classie.toggleClass( target, 'gigante' );
+
+    if ( isGigante ) {
+      // if shrinking, just layout
+      pckry.layout();
+    } else {
+      // if expanding, fit it
+      pckry.fit( target );
+    }
+  });
 });
+
+
+
+
