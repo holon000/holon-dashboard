f6bd5eb677ee16c78035eff12b9dd15bb353577b
Taylor Gerring <taylor.gerring@gmail.com>
2014-02-15 14:41:36 +0100

    Made Draggabilly toggleable via CSS class "draggable"

--- assets/linker/js/main.js
+++ assets/linker/js/main.js
@@ -10,13 +10,14 @@ docReady( function() {
   });
 
   var itemElems = pckry.getItemElements();
-  // for each item element
   for ( var i=0, len = itemElems.length; i < len; i++ ) {
     var elem = itemElems[i];
-    // make element draggable with Draggabilly
-    var draggie = new Draggabilly( elem );
-    // bind Draggabilly events to Packery
-    pckry.bindDraggabillyEvents( draggie );
+    if (elem.classList.contains('draggable')) {
+      // make element draggable with Draggabilly
+      var draggie = new Draggabilly( elem );
+      // bind Draggabilly events to Packery
+      pckry.bindDraggabillyEvents( draggie );
+    }
   }
 
   eventie.bind( container, 'click', function( event ) {
